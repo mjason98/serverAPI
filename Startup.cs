2b9f684d50a3b8cdfb3f1c7c644013ad4adf4156
mjason <mjasoncuba@gmail.com>
2021-11-17 18:30:42 -0500

    added cors options to any

--- Startup.cs
+++ Startup.cs
@@ -53,6 +53,8 @@ namespace serverAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
