8728b6e19312fa6555a32ceeb6d86a06f9c888f9
mjason <mjasoncuba@gmail.com>
2021-12-03 19:40:24 -0500

    added AuthController for autentification API calls

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace serverAPI.Controllers{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase {
+        public AuthController (){
+            
+        }
+    }
+}
